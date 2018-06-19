82da4af790b84623351619435c19ff534d6aaa2c
amol.singh <amol.singh@DPCD-8CG8110BB3.espire.com>
2018-06-19 15:42:19 +0530

    static resources

--- taxi/src/main/java/com/online/booking/config/ApplicationConfig.java
+++ taxi/src/main/java/com/online/booking/config/ApplicationConfig.java
@@ -1,65 +1,42 @@
 package com.online.booking.config;
 
-import java.util.Properties;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.servlet.mvc.UrlFilenameViewController;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({ "com.online.booking" })
 @PropertySource(value = { "classpath:public.properties" })
-public class ApplicationConfig 
+public class ApplicationConfig extends WebMvcConfigurerAdapter
 {
-
-//	@Autowired
-//	private Environment env;
-
-//	@Bean
-//	public ViewResolver jspViewResolver() {
-//		UrlBasedViewResolver resolver = new ChainableUrlBasedViewResolver();
-//		resolver.setPrefix("/resources/HTML/");
-//		resolver.setSuffix(".html");
-//		resolver.setOrder(0);
-//		return resolver;
-//	}
-//	
-//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/HTML/**").addResourceLocations("/HTML/");
-//    }
 	
 	@Bean
 	public ViewResolver getViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		resolver.setOrder(1);
+		resolver.setSuffix("");
 		return resolver;
 	}
-	
-	@Bean(name = "urlViewController")
-    public UrlFilenameViewController getUrlViewController() {
-        UrlFilenameViewController urlViewController = new UrlFilenameViewController();
-        return urlViewController;
-    }
-     
-    @Bean
-    public SimpleUrlHandlerMapping getUrlHandlerMapping() {
-        SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
-        Properties mappings = new Properties();
-        mappings.put("/**/*.html", "urlViewController");
-         
-        handlerMapping.setMappings(mappings);
-        return handlerMapping;
-    }
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/assets/**") //
+				.addResourceLocations("/assets/").setCachePeriod(31556926);
+
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 }
--- taxi/src/main/java/com/online/booking/controller/TestController2.java
+++ taxi/src/main/java/com/online/booking/controller/TestController2.java
@@ -2,14 +2,19 @@ package com.online.booking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/testApi2")
 @Controller
 public class TestController2 
 {
 	@RequestMapping("/showHomePage")
-    public ModelAndView showHome(){
-    	return new ModelAndView("about");
+    public String showHome(){
+    	return "/static/about.html";
     }
+	
+	@RequestMapping("/testStaticResources")
+    public String showCss(){
+    	return "/assets/css/icons.css";
+    }
+	
 }
