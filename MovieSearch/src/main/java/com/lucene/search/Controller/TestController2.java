eabe63199ac25d801656f5da34d62fab4eac656e
lenovo <amolsingh9372@gmail.com>
2018-08-20 08:23:18 +0530

    google sign in

--- MovieSearch/src/main/java/com/lucene/search/Controller/TestController2.java
+++ MovieSearch/src/main/java/com/lucene/search/Controller/TestController2.java
@@ -3,7 +3,9 @@ package com.lucene.search.Controller;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +22,21 @@ public class TestController2
 	    	jsonArray.put(json);
 	    }
 	    return jsonArray.toString();
+	}
+	
+	@RequestMapping(value ="/messages/{id}", method = RequestMethod.GET)
+	public void displayId(@PathVariable("id") int id){
+		System.out.println("Inside messages GET API , Id : "+id);
+	}
+	
+	@RequestMapping(value ="/messages/{id}", method = RequestMethod.DELETE)
+	public void deleteId(@PathVariable("id") int id){
+		System.out.println("Inside messages DELETE API , Id : "+id);
+	}
+	
+	@RequestMapping(value ="/messages/{id}", method = RequestMethod.PUT)
+	public void updateId(@PathVariable("id") int id){
+		System.out.println("Inside messages PUT API , Id : "+id);
 	}
 	
 }
