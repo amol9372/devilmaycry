ae52b645a9e67b88fcb541f6d39740ae85b192c0
Amol Singh <amol.singh@espire.com>
2018-10-16 16:42:08 +0530

    Method refernce practice

--- Java8/src/main/java/com/practice/Java8/GenericCalCulator.java
+++ Java8/src/main/java/com/practice/Java8/GenericCalCulator.java
@@ -1,7 +1,24 @@
 package com.practice.Java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@FunctionalInterface
 interface Operation<T,V>{
+	
 	V mathOperation(T t1, T t2);
+	
+}
+
+@FunctionalInterface
+interface FuncInt<S>{
+	String lambdaExp(S s);
+}
+
+class Sum {
+	Integer doSum(String s1, String s2) {
+		return Integer.parseInt(s1) + Integer.parseInt(s1);
+	}
 }
 
 public class GenericCalCulator
@@ -11,6 +28,12 @@ public class GenericCalCulator
 		Operation<Integer,Integer> subtraction = (a, b) -> { return (a-b); };
 
 		System.out.println(sum.mathOperation(3, 70));
+		
+		List<String> streamList = new ArrayList<>();
+		
+		long x = streamList.stream().filter(String::isEmpty).count();
+		
+		
 		
 	 }
 }
