dd7c90ab2f9da07551d6de624c082d2399623477
Amol Singh <amol.singh@espire.com>
2018-12-10 17:48:31 +0530

    power bi

---
+++ sample-app/src/app/powerbi/powerbi.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as pbi from 'powerbi-client';
+
+@Component({
+  selector: 'app-powerbi',
+  templateUrl: './powerbi.component.html',
+  styleUrls: ['./powerbi.component.css']
+})
+export class PowerbiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
