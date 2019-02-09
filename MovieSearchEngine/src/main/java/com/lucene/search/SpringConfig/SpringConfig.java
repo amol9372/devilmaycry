da0331bfebcd44fbce280a918954d3d7bb24f264
amol9372 <amolsingh9372@gmail.com>
2019-02-09 22:59:42 +0530

    movir project

---
+++ MovieSearchEngine/src/main/java/com/lucene/search/SpringConfig/SpringConfig.java
@@ -0,0 +1,78 @@
+package com.lucene.search.SpringConfig;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.lucene.search.Model.GoogleSignInProperties;
+import com.lucene.search.Model.PersonModel;
+import com.opencsv.CSVReader;
+import com.practice.LuceneSearch.practice.LuceneDemo;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "com.lucene.search" })
+@PropertySource(value = { "classpath:public.properties" })
+public class SpringConfig
+{
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	// @Override
+	// public void
+	// configureDefaultServletHandling(DefaultServletHandlerConfigurer
+	// configurer) {
+	// configurer.enable();
+	// }
+	
+	
+	/* initializing google parameters */
+	
+     @Bean
+     public GoogleSignInProperties getGoogleSignInProperties(){
+    	 GoogleSignInProperties googleSignInProperties = new GoogleSignInProperties();
+    	 googleSignInProperties.setClient_id(env.getProperty("client_id"));
+    	 googleSignInProperties.setRedirect_uri(env.getProperty("redirect_uri"));
+    	 googleSignInProperties.setApproval_prompt(env.getProperty("approval_prompt"));
+    	 googleSignInProperties.setResponse_id(env.getProperty("response_id"));
+    	 googleSignInProperties.setScope(env.getProperty("scope"));
+    	 googleSignInProperties.setClient_secret(env.getProperty("client_secret"));
+    	 googleSignInProperties.setGrant_type(env.getProperty("grant_type"));
+    	 return googleSignInProperties;
+     }
+     
+     public String getProperty(String key){
+ 		return env.getProperty(key);
+ 	}
+     
+    @Bean
+    public LuceneDemo getLuceneDemo() throws FileNotFoundException{
+    	CSVReader csvReader = new CSVReader(
+				new FileReader("C:\\Users\\amol.singh\\Desktop\\movies dataset\\name.basics.csv"), ',');
+		LuceneDemo luceneDemo = new LuceneDemo();
+		luceneDemo.indexCSVDocuments(csvReader);
+		return luceneDemo;
+    }
+     
+    public PersonModel getPersonModel() {
+    	return new PersonModel("Amol", 1990, 0, Arrays.asList("Doctor","Carpenter","Enginner"));
+    }
+}
