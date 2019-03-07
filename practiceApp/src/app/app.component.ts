d6d65f6af536eda7b614b5a9e109d34ac9b2a693
amol9372 <amolsingh9372@gmail.com>
2019-03-07 16:59:49 +0530

    dnd image

--- practiceApp/src/app/app.component.ts
+++ practiceApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Person } from './person';
+import { DragImage } from 'ng2-dnd';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,18 @@ import { Person } from './person';
 export class AppComponent {
     restrictedDrop1: any = null;
     restrictedDrop2: any = null;
+    image : DragImage;
+
+    ngOnInit(){
+      var img1 = new Image();
+      img1.src = '/assets/images/drag.png';
+      img1.width = 10;
+      console.log(img1.naturalHeight)
+      img1.height = 10;
+      this.image = new DragImage(img1, 0, 0);
+    }
+
+   // var img1 = new Image();
+   // img1.src = '/images/drag.png';
+   // image = new DragImage('/images/drag.png', 0, 0);;
 }
