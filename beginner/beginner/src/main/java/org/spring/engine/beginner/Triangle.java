a23b4cb416f9e5e9c9e20bc0f799d43ad8b8b62b
Preet K. Kaur <pkkaur@bravurasolutions.com>
2019-04-26 23:35:48 +0530

    Object & constructor injection

--- beginner/beginner/src/main/java/org/spring/engine/beginner/Triangle.java
+++ beginner/beginner/src/main/java/org/spring/engine/beginner/Triangle.java
@@ -2,18 +2,37 @@ package org.spring.engine.beginner;
 
 public class Triangle {
 
-	private String type;
-	
-	public String getType() {
-		return type;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw()
 	{
-		System.out.println(getType() + " Triangle drawn!!");
+		System.out.println("Point A " + getPointA().getX() + getPointA().getY() + "::: point B" + getPointB().getX() + getPointB().getY()
+				+ "::: Point C" + getPointC().getX() + getPointC().getY());
 	}
 }
