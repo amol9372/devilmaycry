0df93e9b6badf44f3077d22402eb9f2c19440560
amol9372 <amolsingh9372@gmail.com>
2019-05-01 11:11:27 +0530

    kjadq

--- practiceApp/src/app/app.component.ts
+++ practiceApp/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Person } from './person';
 import { DragImage } from 'ng2-dnd';
+import { UploadEvent, UploadFile, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,12 @@ export class AppComponent {
     restrictedDrop1: any = null;
     restrictedDrop2: any = null;
     image : DragImage;
-
+    public files: UploadFile[] = [];
+    
     ngOnInit(){
       var img1 = new Image();
       img1.src = '/assets/images/drag.png';
-      img1.width = 10;
+      
       console.log(img1.naturalHeight)
       img1.height = 10;
       this.image = new DragImage(img1, 0, 0);
@@ -28,4 +30,51 @@ export class AppComponent {
    // var img1 = new Image();
    // img1.src = '/images/drag.png';
    // image = new DragImage('/images/drag.png', 0, 0);;
+
+   
+ 
+   public dropped(event: UploadEvent) {
+    this.files = event.files;
+    for (const droppedFile of event.files) {
+ 
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+ 
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+ 
+          /**
+          // You could upload it like this:
+          const formData = new FormData()
+          formData.append('logo', file, relativePath)
+ 
+          // Headers
+          const headers = new HttpHeaders({
+            'security-token': 'mytoken'
+          })
+ 
+          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+          .subscribe(data => {
+            // Sanitized logo returned from backend
+          })
+          **/
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event){
+    console.log(event);
+  }
+
+  public fileLeave(event){
+    console.log(event);
+  }
 }
--- practiceApp/src/app/app.module.ts
+++ practiceApp/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
 import {DndModule} from 'ng2-dnd';
-
+import { FileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, DragDropModule, DndModule.forRoot()
+    BrowserModule, DragDropModule, DndModule.forRoot(), FileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
