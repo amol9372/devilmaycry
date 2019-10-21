6251f5f64fcc8ed1aeb05f7ca7bd3ff5134ac4ab
amol9372 <amolsingh9372@gmail.com>
2019-10-21 18:36:24 +0530

    Project setup

--- Geocode/src/main/java/com/places/geocode/service/GeocodeService.java
+++ Geocode/src/main/java/com/places/geocode/service/GeocodeService.java
@@ -2,7 +2,13 @@ package com.places.geocode.service;
 
 import org.springframework.stereotype.Service;
 
+import com.places.geocode.model.GeocodeAddressResponse;
+
 @Service
 public class GeocodeService {
    
+	public GeocodeAddressResponse getLatLongFromAddress() {
+		return null;
+	}
+	
 }
