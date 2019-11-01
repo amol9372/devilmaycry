0b390cfb3e23849ab6f3f62c0f5221f44eac68c0
amol9372 <amolsingh9372@gmail.com>
2019-11-01 17:24:03 +0530

    slkdnfwdf

--- MyPlacesUI/src/app/app.module.ts
+++ MyPlacesUI/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { FilterPipe } from './filter.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './imports/material.module';
+import { SearchService } from './search.service';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    FilterPipe,
+    ReactiveFormsModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- MyPlacesUI/src/app/search/search.component.ts
+++ MyPlacesUI/src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  url = 'https://jsonplaceholder.typicode.com/users';
+  usersArray: any[] = [];
 
+  /*  constructor(private http: HttpClient) {
+     this.http.get(this.url).subscribe((data: any[]) => {
+       // Populating usersArray with names from API
+       data.forEach(element => {
+         this.usersArray.push(element.name);
+       });
+       console.log(data);
+     });
+   } */
+
+  results: any[] = [];
+  queryField: FormControl = new FormControl();
+  constructor(private _searchService: SearchService) { }
   ngOnInit() {
+    this.queryField.valueChanges
+      .subscribe(result => console.log(result));
   }
 
 }
