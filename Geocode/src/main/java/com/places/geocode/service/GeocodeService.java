449d6e562906fa713b647f72ff0ffecd9350db37
amol9372 <amolsingh9372@gmail.com>
2019-11-13 18:26:51 +0530

    h2 select

--- Geocode/src/main/java/com/places/geocode/service/GeocodeService.java
+++ Geocode/src/main/java/com/places/geocode/service/GeocodeService.java
@@ -1,17 +1,15 @@
 package com.places.geocode.service;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import com.places.Utility.Enums.Status;
+import com.places.Utility.Exceptions.PlaceNotFoundException;
+import com.places.geocode.dao.GeocodePlaceRepository;
 import com.places.geocode.model.GeocodeAddressResponse;
 import com.places.geocode.model.PlaceModel;
 import com.places.geocode.service.impl.GeocodeServiceImpl;
@@ -22,6 +20,9 @@ public class GeocodeService {
 	@Autowired
 	private GeocodeServiceImpl geocodeServiceImpl;
 	
+	@Autowired
+	private GeocodePlaceRepository geocodePlaceRepository;
+	
 	public GeocodeAddressResponse getLatLongFromAddress(String address) throws RestClientException, UnsupportedEncodingException {
 		return geocodeServiceImpl.getLatLong(address);
 	}
@@ -30,6 +31,13 @@ public class GeocodeService {
 		return geocodeServiceImpl.getPlaceList();
 	}
 	
-	
+	public PlaceModel getPlace(int id) throws PlaceNotFoundException {
+		Optional<PlaceModel> placeModelOptional = geocodePlaceRepository.findById(id);
+		if(!placeModelOptional.isPresent()) {
+			throw new PlaceNotFoundException("Place Not found");
+		}
+		
+		return placeModelOptional.get();
+	}
 	
 }
