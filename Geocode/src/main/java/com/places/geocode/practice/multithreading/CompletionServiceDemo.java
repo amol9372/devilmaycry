b36d9af41c3b8fb9a2f59433a7844524ac91dddb
amol9372 <amolsingh9372@gmail.com>
2019-12-18 09:48:25 +0530

    threading

--- Geocode/src/main/java/com/places/geocode/practice/multithreading/CompletionServiceDemo.java
+++ Geocode/src/main/java/com/places/geocode/practice/multithreading/CompletionServiceDemo.java
@@ -2,9 +2,9 @@ package com.places.geocode.practice.multithreading;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class CompletionServiceDemo {
@@ -13,10 +13,10 @@ public class CompletionServiceDemo {
 		
         //CompletableFuture<String> nameFuture = CompletableFuture.supplyAsync(() -> "Amol").thenApplyAsync(s -> s.concat(" Singh"));
 		//System.out.println(nameFuture.get());
-		
+    	Executor executor = Executors.newFixedThreadPool(2);
 		Runnable count1To11 = () ->  {  IntStream.range(1, 11).forEach(i -> System.out.println(Thread.currentThread().getName() + " " + i + " "));  } ;
 		
-		Runnable count11To21 = () ->  {  IntStream.range(11, 21).forEach(i -> System.out.println(Thread.currentThread().getName() + " " + i + " "));  } ;
+		Runnable count11To21 = () ->  {  IntStream.range(11, 21).forEach(i -> System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().isDaemon() + " " + i + " "));  } ;
 		
 		Runnable sleepingTask = () -> {
 			try {
@@ -28,25 +28,17 @@ public class CompletionServiceDemo {
 			}
 		};
 		
-		CompletableFuture<Void> countFuture = CompletableFuture.runAsync(count1To11);
+		//CompletableFuture<Void> countFuture = CompletableFuture.runAsync(sleepingTask, executor);
 		
-		//Thread.sleep(5000);
-		//System.out.println("Main thread is running .... ");
-		//countFuture.complete(null);
+		CompletableFuture.supplyAsync(() -> {
+		    return "Some result";
+		}).thenApplyAsync(result -> {
+		    // Executed in a thread obtained from the executor
+		    return "Processed Result";
+		}, executor);
 		
 		
 	} 
-    
-    public static Future<String> calculateAsync() throws InterruptedException {
-        CompletableFuture<String> completableFuture = new CompletableFuture<>();
-     
-        Executors.newCachedThreadPool().submit(() -> {
-            Thread.sleep(500);
-            completableFuture.complete("Hello");
-            return null;
-        });
-        return completableFuture;
-    }
    
 }
 
