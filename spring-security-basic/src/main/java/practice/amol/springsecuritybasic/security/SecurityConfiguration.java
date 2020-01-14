84a4da8ef1801d7c24d17928a09638e9e6675632
amol9372 <amolsingh9372@gmail.com>
2020-01-14 19:29:10 +0530

    security using jdbc

--- spring-security-basic/src/main/java/practice/amol/springsecuritybasic/security/SecurityConfiguration.java
+++ spring-security-basic/src/main/java/practice/amol/springsecuritybasic/security/SecurityConfiguration.java
@@ -1,33 +1,48 @@
 package practice.amol.springsecuritybasic.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	    auth.inMemoryAuthentication()
-	         .withUser("amol")
-	         .password("singh")
-	         .roles("USER")
-	         .and()
-	         .withUser("admin")
-	         .password("admin")
-	         .roles("ADMIN");
+		/*
+		 * auth.inMemoryAuthentication() .withUser("amol") .password("singh")
+		 * .roles("USER") .and() .withUser("admin") .password("admin") .roles("ADMIN");
+		 */
+		auth.jdbcAuthentication()
+		    .dataSource(dataSource)
+		    .withDefaultSchema()
+		    .withUser(
+		    		  User.withUsername("amol")
+		    		      .password("singh")
+		    		      .roles("USER")
+		    ).withUser(
+		    		  User.withUsername("admin")
+	    		      .password("singh")
+	    		      .roles("ADMIN")
+	        );	     
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
        http.authorizeRequests()
-           .antMatchers("/admin").hasRole("ADMIN")
-           .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+           .antMatchers("/user").hasRole("ADMIN")
+           .antMatchers("/admin").hasAnyRole("USER", "ADMIN")
            .and().formLogin();
 	}
 	
