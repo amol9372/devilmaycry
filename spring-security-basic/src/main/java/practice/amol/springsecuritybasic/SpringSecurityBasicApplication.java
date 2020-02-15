d4a620f2a41941bbb2cf71f1eeccddc9c4130b2c
amol9372 <amolsingh9372@gmail.com>
2020-02-15 09:18:15 +0530

    security

--- spring-security-basic/src/main/java/practice/amol/springsecuritybasic/SpringSecurityBasicApplication.java
+++ spring-security-basic/src/main/java/practice/amol/springsecuritybasic/SpringSecurityBasicApplication.java
@@ -2,12 +2,14 @@ package practice.amol.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import practice.amol.springsecuritybasic.repository.UserRepository;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class )
+@PropertySource("file:config.properties")
 public class SpringSecurityBasicApplication {
 
 	public static void main(String[] args) {
