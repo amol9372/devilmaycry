e08a6f00624e8d29aba0f13358ac7d1f72011978
amol9372 <amolsingh9372@gmail.com>
2020-02-17 09:52:59 +0530

    eureka server

--- currency-conversion-service/src/main/java/com/places/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/places/currencyconversionservice/CurrencyConversionController.java
@@ -1,21 +1,14 @@
 package com.places.currencyconversionservice;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-
-	@Autowired
-	private RestTemplate restTemplate;
 	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
